c4f5696819876630bf1b80d43e6324c9fe293024
Takayuki Matsuoka <takayuki.matsuoka@gmail.com>
2023-07-11 08:15:09 +0900

    Add examples/

---
+++ examples/xxh3-128bits-stream.c
@@ -0,0 +1,116 @@
+/*
+How to use XXH3 128bits variant streaming hashing function
+==========================================================
+
+
+Building
+--------
+
+cc -std=c99 -Wall -Wextra -Wundef -Wshadow -Wcast-align -Werror -I .. -L .. xxh3-128bits-stream.c -l xxhash
+
+./a.out ../cli/COPYING
+
+Output
+------
+
+0x445635c86205ac5626ffd8d23b61ee2f  ../cli/COPYING
+
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <xxhash.h>
+
+// Print hash value via canonical struct
+void my_print_xxh128(XXH128_hash_t hashValue)
+{
+    XXH128_canonical_t c;
+    XXH128_canonicalFromHash(&c, hashValue);
+
+    printf("0x");
+    for (size_t i = 0; i < sizeof(c.digest); ++i) {
+        printf("%02x", c.digest[i]);
+    }
+}
+
+// Compute XXH3 128bits variant hash with streaming API
+XXH128_hash_t my_compute_xxh3_128bits_streaming(FILE *fp, char *buf, size_t bufSizeInBytes)
+{
+    // Create state.  Abort if error.
+    XXH3_state_t *const state = XXH3_createState();
+    if (state == NULL) {
+        fprintf(stderr, "Failed : XXH3_createState()\n");
+        abort();
+    }
+
+    // Reset the state.  Abort if error.
+    if (XXH3_128bits_reset(state) == XXH_ERROR) {
+        fprintf(stderr, "Failed : XXH3_128bits_reset(%p)\n", state);
+        abort();
+    }
+
+    // While file stream doesn't reach the end of file,
+    while (!feof(fp)) {
+        // Read the file stream partially
+        size_t const length = fread(buf, 1, bufSizeInBytes, fp);
+        // Update the state with data.  Abort if error.
+        if (XXH3_128bits_update(state, buf, length) == XXH_ERROR) {
+            fprintf(stderr, "Failed : XXH3_128bits_update(%p, %p, %zd)\n", state, buf, length);
+            abort();
+        }
+    }
+
+    // Retrieve digest hash from the state
+    XXH128_hash_t const digest3_128 = XXH3_128bits_digest(state);
+
+    // Free the state.
+    XXH3_freeState(state);
+
+    return digest3_128;
+}
+
+// Compute XXH3 128bits variant hash from content of filename
+XXH128_hash_t my_compute_xxh3_128bits_from_file(const char *filename)
+{
+    // Open file stream.  Abort if error.
+    FILE *const fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Failed : fopen(%s)\n", filename);
+        abort();
+    }
+
+    // Allocate file buffer.  Abort if error.
+    // Note : allocation size is extremely small to demonstrate how streaming API works.
+    size_t const bufSizeInBytes = 1024;
+    char *const  buf            = (char *)malloc(bufSizeInBytes);
+    if (buf == NULL) {
+        fprintf(stderr, "Failed : malloc(%zd)\n", bufSizeInBytes);
+        abort();
+    }
+
+    // Compute XXH128 hash with streaming API
+    XXH128_hash_t const digest3_128 = my_compute_xxh3_128bits_streaming(fp, buf, bufSizeInBytes);
+
+    // Free the file buffer
+    free(buf);
+
+    // Close the file stream
+    fclose(fp);
+
+    return digest3_128;
+}
+
+int main(int argc, char *argv[])
+{
+    // For all arguments
+    for (int i = 1; i < argc; ++i) {
+        const char *filename = argv[i];
+
+        // Compute XXH3 128bits variant hash with streaming API
+        XXH128_hash_t const digest3_128 = my_compute_xxh3_128bits_from_file(filename);
+
+        // Display hash and filename
+        my_print_xxh128(digest3_128);
+        printf("  %s\n", filename);
+    }
+    return EXIT_SUCCESS;
+}
---
+++ examples/xxh64-one-shot.c
@@ -0,0 +1,88 @@
+/*
+How to use XXH64() one-shot hashing function
+============================================
+
+
+Building
+--------
+
+cc -std=c99 -Wall -Wextra -Wundef -Wshadow -Wcast-align -Werror -I .. -L .. xxh64-one-shot.c -l xxhash
+
+
+Output
+------
+
+data[38] = {
+    0x78, 0x78, 0x48, 0x61, 0x73, 0x68, 0x20, 0x2d,
+    0x20, 0x45, 0x78, 0x74, 0x72, 0x65, 0x6d, 0x65,
+    0x6c, 0x79, 0x20, 0x66, 0x61, 0x73, 0x74, 0x20,
+    0x68, 0x61, 0x73, 0x68, 0x20, 0x61, 0x6c, 0x67,
+    0x6f, 0x72, 0x69, 0x74, 0x68, 0x6d,
+}
+digest64 = 0xd80fe6cd6d6402e9
+
+ */
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <xxhash.h>
+
+// Print data
+void my_print_data(const void *data, size_t dataSizeInBytes)
+{
+    const uint8_t *const d = (const uint8_t *)data;
+    for (size_t i = 0; i < dataSizeInBytes; ++i) {
+        if (i % 8 == 0) {
+            printf("    ");
+        }
+        printf("0x%02x, ", d[i]);
+        if (i % 8 == 7 || i + 1 == dataSizeInBytes) {
+            printf("\n");
+        }
+    }
+}
+
+// Print hash value via canonical struct
+void my_print_xxh64(XXH64_hash_t hashValue)
+{
+    XXH64_canonical_t c;
+    XXH64_canonicalFromHash(&c, hashValue);
+
+    printf("0x");
+    for (size_t i = 0; i < sizeof(c.digest); ++i) {
+        printf("%02x", c.digest[i]);
+    }
+}
+
+// Compute XXH64 variant hash
+void my_compute_xxh64_one_shot()
+{
+    // Setup data
+    const char *data            = "xxHash - Extremely fast hash algorithm";
+    size_t      dataSizeInBytes = strlen(data);
+
+    // Display data
+    printf("data[%zd] = {\n", dataSizeInBytes);
+    my_print_data(data, dataSizeInBytes);
+    printf("}\n");
+
+    // Compute hash
+    XXH64_hash_t digest64 =  //
+        XXH64(               // 64-bit one-shot hash function
+            data,            // pointer to data
+            dataSizeInBytes, // data length in bytes
+            0ULL             // 64-bit seed
+        );
+
+    // Display hash
+    printf("digest64 = ");
+    my_print_xxh64(digest64);
+    printf("\n");
+}
+
+int main()
+{
+    my_compute_xxh64_one_shot();
+    return EXIT_SUCCESS;
+}
